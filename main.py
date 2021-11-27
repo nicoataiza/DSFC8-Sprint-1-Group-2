29a7d586e0a79f13feb62f3f463ac1a8611df8a7
sofiaubaldo <masofiaubaldo@gmail.com>
2021-11-28 06:58:00 +0800

    updated main.py

--- main.py
+++ main.py
@@ -128,6 +128,15 @@ def background():
 
 def data_method():
     st.title('Data Sources and Methodology')
+    st.write("")
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        data_sources = Image.open("assets/Data Source.JPG")
+        st.image(data_sources)
+    with col2:
+        methodology = Image.open("assets/Methodology2.JPG")
+        st.image(methodology)
+    
     st.subheader("Data Sources")
 
     st.write("")
@@ -141,115 +150,140 @@ def data_method():
 
 
 def win_loss():
+    st.title("What did Winners do differently?")
     option = st.selectbox('Analysis on:', ['Social Media Presence','Electoral Surveys', 'Contributions', 'Expenditures'])
 
     if option == 'Social Media Presence':
-        st.markdown('The Philippines ranked first in internet and social media usage last 2020')
-        st.markdown('The average Filipino is on social media for around 3 hours and 50 minutes daily')
-
-        # Load the data
-        campaign = load_data(option="2019-campaigns")
-        votes = load_data(option="2019-senatorial-votes")
-
-        # Merge the data
-        df_merged = pd.merge(left=campaign, right=votes, how='left', on='Candidate')
-
-        # Fill nulls
-        df_merged.fillna(0, inplace=True)
-
-        # Perform aggregations
-        df_merged['Twitter - Total Interactions'] = (
-                                                            df_merged['Twitter - Number of Mentions']
-                                                            + df_merged['Twitter - Number of Users']
-                                                            + df_merged['Twitter- Total Favorites']
-                                                            + df_merged['Twitter - Total Retweets']
-                                                            + df_merged['Twitter - Total Replies']
-                                                    ) / 1000000
-        df_merged['Facebook - Total Interactions'] = (
-                                                             df_merged['Facebook - Number of Candidate Posts']
-                                                             + df_merged['Facebook - Total Comments']
-                                                             + df_merged['Facebook - Total Shares']
-                                                             + df_merged['Facebook - Total Reactions']
-                                                     ) / 1000000
-        df_merged['Total Social Media Interactions'] = df_merged['Twitter - Total Interactions'] \
-                                                       + df_merged['Facebook - Total Interactions']
-
-        # Generate mapping function
-        def win_category(x):
-            if x == 1:
-                return 'Win'
-            else:
-                return 'Lose'
-
-        # Map function: 1 to Win and 0 to Lose
-        df_merged['Win-Category'] = df_merged['Win'].apply(lambda x: win_category(x))
-
-        # Sort the data
-        df_merged.sort_values(by='Votes', ascending=False, inplace=True)
-
-        # Generate scatter plot
-        fig = plt.figure(figsize=(12, 8), dpi=150)
-        sns.scatterplot(y='Votes', x='Total Social Media Interactions', data=df_merged, hue='Win-Category', s=150)
-        sns.despine()
-        plt.title('Votes vs. Social Media Interactions', size=18)
-        plt.xlabel('Social Media Interactions - in millions', size=12)
-        plt.ylabel('Votes', size=12)
-        st.pyplot(fig)
         
+        col1, col2 = st.beta_columns([5,11])
+        with col1:
+            st.markdown('The Philippines ranked first in internet and social media usage last 2020')
+            st.markdown('The average Filipino is on social media for around 3 hours and 50 minutes daily')
+
+        with col2:
+            # Load the data
+            campaign = load_data(option="2019-campaigns")
+            votes = load_data(option="2019-senatorial-votes")
+
+            # Merge the data
+            df_merged = pd.merge(left=campaign, right=votes, how='left', on='Candidate')
+
+            # Fill nulls
+            df_merged.fillna(0, inplace=True)
+
+            # Perform aggregations
+            df_merged['Twitter - Total Interactions'] = (
+                                                                df_merged['Twitter - Number of Mentions']
+                                                                + df_merged['Twitter - Number of Users']
+                                                                + df_merged['Twitter- Total Favorites']
+                                                                + df_merged['Twitter - Total Retweets']
+                                                                + df_merged['Twitter - Total Replies']
+                                                        ) / 1000000
+            df_merged['Facebook - Total Interactions'] = (
+                                                                 df_merged['Facebook - Number of Candidate Posts']
+                                                                 + df_merged['Facebook - Total Comments']
+                                                                 + df_merged['Facebook - Total Shares']
+                                                                 + df_merged['Facebook - Total Reactions']
+                                                         ) / 1000000
+            df_merged['Total Social Media Interactions'] = df_merged['Twitter - Total Interactions'] \
+                                                           + df_merged['Facebook - Total Interactions']
+
+            # Generate mapping function
+            def win_category(x):
+                if x == 1:
+                    return 'Win'
+                else:
+                    return 'Lose'
+
+            # Map function: 1 to Win and 0 to Lose
+            df_merged['Win-Category'] = df_merged['Win'].apply(lambda x: win_category(x))
+
+            # Sort the data
+            df_merged.sort_values(by='Votes', ascending=False, inplace=True)
+
+            # Generate scatter plot
+            fig = plt.figure(figsize=(10, 6), dpi=150)
+            sns.scatterplot(y='Votes', x='Total Social Media Interactions', data=df_merged, hue='Win-Category', s=150)
+            sns.despine()
+            plt.title('Votes vs. Social Media Interactions', size=18)
+            plt.xlabel('Social Media Interactions - in millions', size=12)
+            plt.ylabel('Votes', size=12)
+            st.pyplot(fig)
+
     elif option == 'Electoral Surveys':
-        
-        # Load the data
-        df2 = load_data(option="2019-surveys")  
-        
-        # Generate mapping function
-        def win_category(x):
-            if x == 1:
-                return 'Win'
-            else:
-                return 'Lose'
+        col1, col2 = st.beta_columns([5,11])
+        with col1:
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("It is wise for the candidates to track their survey results as it is highly correlated with actual votes.")
+            st.write("")
 
-        # Map function: 1 to Win and 0 to Lose
-        df2['Win Category'] = df2['Win'].apply(lambda x: win_category(x))
-        
-        # Plot scatter plots of electoral surveys vs votes
-        
-        fig1 = plt.figure(figsize=(10, 8))
-        sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (Jan 26-31)", data=df2, hue="Win Category")
-        plt.title("Electoral Survey (Jan 26-31, 2019) vs Actual Votes", fontsize=20)
-        plt.xlabel("Actual Votes")
-        plt.ylabel("Electoral Survey %")
-        st.pyplot(fig1)
+        with col2:
+            option = st.selectbox(
+                'Select Date:',
+                ['Jan 26-31, 2019', 'Feb 24-28, 2019', 'Mar 23-27, 2019', 'Apr 10-14, 2019', 'May 3-6, 2019'])
+            
+            # Load the data
+            df2 = load_data(option="2019-surveys")  
 
-        fig2 = plt.figure(figsize=(10, 8))
-        sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (Feb 24-28)", data=df2, hue="Win Category")
-        plt.title("Electoral Survey (Feb 24-28, 2019) vs Actual Votes", fontsize=20)
-        plt.xlabel("Actual Votes")
-        plt.ylabel("Electoral Survey %")
-        st.pyplot(fig2)
+            # Generate mapping function
+            def win_category(x):
+                if x == 1:
+                    return 'Win'
+                else:
+                    return 'Lose'
 
-        fig3 = plt.figure(figsize=(10, 8))
-        sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (Mar 23-27)", data=df2, hue="Win Category")
-        plt.title("Electoral Survey (Mar 23-27, 2019) vs Actual Votes", fontsize=20)
-        plt.xlabel("Actual Votes")
-        plt.ylabel("Electoral Survey %")
-        st.pyplot(fig3)
+            # Map function: 1 to Win and 0 to Lose
+            df2['Win Category'] = df2['Win'].apply(lambda x: win_category(x))
 
-        fig4 = plt.figure(figsize=(10, 8))
-        sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (Apr 10-14)", data=df2, hue="Win Category")
-        plt.title("Electoral Survey (Apr 10-14, 2019) vs Actual Votes", fontsize=20)
-        plt.xlabel("Actual Votes")
-        plt.ylabel("Electoral Survey %")
-        st.pyplot(fig4)
+            # Plot scatter plots of electoral surveys vs votes
+            if option == "Jan 26-31, 2019":
+                fig1 = plt.figure(figsize=(10, 6))
+                sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (Jan 26-31)", data=df2, hue="Win Category")
+                plt.title("Electoral Survey (Jan 26-31, 2019) vs Actual Votes", fontsize=20)
+                plt.xlabel("Actual Votes")
+                plt.ylabel("Electoral Survey %")
+                st.pyplot(fig1)
 
-        fig5 = plt.figure(figsize=(10, 8))
-        sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (May 3-6)", data=df2, hue="Win Category")
-        plt.title("Electoral Survey (May 3-6, 2019) vs Actual Votes", fontsize=20)
-        plt.xlabel("Actual Votes")
-        plt.ylabel("Electoral Survey %")
-        st.pyplot(fig5)
-        
-        # Compute correlations of votes and surveys
-        df2.corr()
+            elif option == "Feb 24-28, 2019":
+                fig2 = plt.figure(figsize=(10, 6))
+                sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (Feb 24-28)", data=df2, hue="Win Category")
+                plt.title("Electoral Survey (Feb 24-28, 2019) vs Actual Votes", fontsize=20)
+                plt.xlabel("Actual Votes")
+                plt.ylabel("Electoral Survey %")
+                st.pyplot(fig2)
+
+            elif option == "Mar 23-27, 2019":
+                fig3 = plt.figure(figsize=(10, 6))
+                sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (Mar 23-27)", data=df2, hue="Win Category")
+                plt.title("Electoral Survey (Mar 23-27, 2019) vs Actual Votes", fontsize=20)
+                plt.xlabel("Actual Votes")
+                plt.ylabel("Electoral Survey %")
+                st.pyplot(fig3)
+
+            elif option == "Apr 10-14, 2019":
+                fig4 = plt.figure(figsize=(10, 6))
+                sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (Apr 10-14)", data=df2, hue="Win Category")
+                plt.title("Electoral Survey (Apr 10-14, 2019) vs Actual Votes", fontsize=20)
+                plt.xlabel("Actual Votes")
+                plt.ylabel("Electoral Survey %")
+                st.pyplot(fig4)
+                
+            elif option == "May 3-6, 2019":
+                fig5 = plt.figure(figsize=(10, 6))
+                sns.scatterplot(x="Votes", y="PulseAsia Survey 2019 (May 3-6)", data=df2, hue="Win Category")
+                plt.title("Electoral Survey (May 3-6, 2019) vs Actual Votes", fontsize=20)
+                plt.xlabel("Actual Votes")
+                plt.ylabel("Electoral Survey %")
+                st.pyplot(fig5)
+
+            # Compute correlations of votes and surveys
+            df2.corr()
 
 
     elif option == 'Contributions':
